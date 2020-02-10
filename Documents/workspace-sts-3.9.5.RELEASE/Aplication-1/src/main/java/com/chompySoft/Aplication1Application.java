f824bf3f3d3989bbbb3427c8cec6cfa7c730b0c8
chompy <andreswittouck@gmail.com>
2020-02-10 19:05:33 -0300

    primer commit. Close #1

---
+++ Documents/workspace-sts-3.9.5.RELEASE/Aplication-1/src/main/java/com/chompySoft/Aplication1Application.java
@@ -0,0 +1,13 @@
+package com.chompySoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aplication1Application.class, args);
+	}
+
+}
